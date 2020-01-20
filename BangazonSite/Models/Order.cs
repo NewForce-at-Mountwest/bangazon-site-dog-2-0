b6cb5ebd7bd542f1fd4ee1f8f1976487e546a318
JACOB <JACOBPRY@AOl.com>
2020-01-20 13:38:04 -0500

    Added Seed Data

--- BangazonSite/Models/Order.cs
+++ BangazonSite/Models/Order.cs
@@ -10,8 +10,7 @@ namespace BangazonSite.Models
     public class Order
     {
         public int Id { get; set; }
-        [Required]
-        public int PaymentTypeId { get; set; }
+        public int? PaymentTypeId { get; set; }
         [Required]
         public string ApplicationUserId { get; set; }
         public PaymentType PaymentType { get; set; }
